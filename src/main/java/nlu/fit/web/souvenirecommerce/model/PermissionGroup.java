90a29f1b75e5df5d9fc85422308bcbdffc145d66
Van Toan <21130572@st.hcmuaf.edu.vn>
2026-04-25 21:49:17 +0700

    fix(authorization): change authorization to use permission groups

---
+++ src/main/java/nlu/fit/web/souvenirecommerce/model/PermissionGroup.java
@@ -0,0 +1,30 @@
+package nlu.fit.web.souvenirecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PermissionGroup implements Serializable {
+    private int id;
+    private String name;
+    private String description;
+    private boolean system;
+
+    @Default
+    private List<PermissionItem> permissions = new ArrayList<>();
+
+    @Default
+    private List<User> users = new ArrayList<>();
+
+    private int userCount;
+}
