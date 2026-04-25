aa02feea65d63306e964c2019cc5a05eddae4a89
Van Toan <21130572@st.hcmuaf.edu.vn>
2026-04-25 21:49:18 +0700

    fix(authorization): change authorization to use permission groups

---
+++ src/main/java/nlu/fit/web/souvenirecommerce/model/PermissionItem.java
@@ -0,0 +1,19 @@
+package nlu.fit.web.souvenirecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PermissionItem implements Serializable {
+    private int id;
+    private String resource;
+    private String action;
+    private String description;
+}
