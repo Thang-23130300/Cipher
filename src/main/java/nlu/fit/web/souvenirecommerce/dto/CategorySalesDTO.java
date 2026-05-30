9fd7d93e01df25cf4da5fe3627bdeee036b64356
Van Toan <vantoan19122003@gmail.com>
2026-05-30 17:42:14 +0700

    feat(admin_dashboard): adding functions to the admin dashboard

---
+++ src/main/java/nlu/fit/web/souvenirecommerce/dto/CategorySalesDTO.java
@@ -0,0 +1,32 @@
+package nlu.fit.web.souvenirecommerce.dto;
+
+public class CategorySalesDTO {
+
+    private String categoryName;
+    private int totalSold;
+    private double revenue;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getTotalSold() {
+        return totalSold;
+    }
+
+    public void setTotalSold(int totalSold) {
+        this.totalSold = totalSold;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(double revenue) {
+        this.revenue = revenue;
+    }
+}
